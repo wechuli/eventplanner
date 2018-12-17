ef25bd5bf1b26a50365ac81bb87e99249888066d
Wechuli <wechulipaul@yahoo.com>
2018-12-17 22:06:16 +0300

    initial files setup

---
+++ revents/src/app/layout/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Re-Vents App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
