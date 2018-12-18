14b0b290d57731f4d43282425c501d537b313250
Wechuli <wechulipaul@yahoo.com>
2018-12-18 09:19:56 +0300

    adding project skeleton

--- revents/src/app/layout/App.jsx
+++ revents/src/app/layout/App.jsx
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-
-
+import React, { Component } from "react";
+import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Re-Vents App</h1>
-      </div>
+      <React.Fragment>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
+      </React.Fragment>
     );
   }
 }
---
+++ revents/src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import {Grid} from 'semantic-ui-react'
+import EventList from '../EventList/EventList';
+
+ class EventDashboard extends Component {
+  render() {
+    return (
+    <Grid>
+        <Grid.Column width={10}>
+            <EventList/>
+        </Grid.Column>
+        <Grid.Column width={6}>
+            <h2>Right Column</h2>
+        </Grid.Column>
+    </Grid>
+    )
+  }
+}
+
+
+export default EventDashboard;
